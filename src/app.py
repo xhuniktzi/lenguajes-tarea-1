c25ea71f835349cfd2ab628da6055dd0ecb1adca
Xhunik Miguel <xhuniktzi@gmail.com>
2021-02-09 16:44:36 -0600

    salida por consola: completo

--- src/app.py
+++ src/app.py
@@ -27,8 +27,19 @@ def clear_ops(ops_list: list):
     op_regex = regex.compile('(BUSCAR|ORDENAR)(\s\d)?')
     for op in ops_list:
         if (type(op) is str) and (op != None) and (op != '\n') and (op != '') and (op != ' '):
-            ops_return.append(op_regex.findall(op))
+            ops_return.append(op_regex.findall(op)[0])
     return ops_return
+
+
+def execute_op(op: tuple, data: list):
+    if op[0] == 'ORDENAR':
+        data.sort()
+        print('Datos ordenados: {}'.format(data))
+    if op[0] == 'BUSCAR':
+        if int(op[1]) in data:
+            print('{} encontrado'.format(int(op[1])))
+        else:
+            print('{} no encontrado'.format(int(op[1])))
 
 
 if __name__ == '__main__':
@@ -37,7 +48,7 @@ if __name__ == '__main__':
 
     for row in file_rows:
         split_row = row.split('=', 1)
-        print(split_row.pop(0))
+        print('Identificador: {}'.format(split_row.pop(0)))
 
         split_op = regex.split('(BUSCAR\s\d$)|(ORDENAR,?)', split_row[0])
 
@@ -45,8 +56,8 @@ if __name__ == '__main__':
         clear_numbers = list(map(lambda string: int(
             string.strip(',')), num_regex.findall(split_op.pop(0))))
 
-        print(clear_numbers)
-
-        print(clear_ops(split_op))
-
-        print('\n')
+        print('Datos: {}'.format(clear_numbers))
+        ops = clear_ops(split_op)
+        for op in ops:
+            execute_op(op, clear_numbers)
+        print()
